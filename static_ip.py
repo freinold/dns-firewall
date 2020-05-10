4496c804b291ddbce6cf9b8f883e83101757d692
freinold <reinold@hm.edu>
2020-05-10 13:32:49 +0200

    Completely changed static_ip.py so it features an own info object instead of a dict.

--- static_ip.py
+++ static_ip.py
@@ -21,6 +21,37 @@ static domain_name_servers={2}
 '''
 
 
+class Info:
+    def __init__(self, filename=None):
+        if filename is None:
+            self.router = None
+            self.subnet = None
+            self.original_ip = None
+            self.original_resolver = None
+            self.static_ip = None
+            self.resolver = None
+        else:
+            with open(filename) as file:
+                info = json.load(file)
+            self.router = ipaddress.ip_address(info["router"])
+            self.subnet = ipaddress.ip_network(info["subnet"])
+            self.original_ip = ipaddress.ip_address(info["original_ip"])
+            self.original_resolver = ipaddress.ip_address(info["original_resolver"])
+            self.static_ip = ipaddress.ip_address(info["static_ip"])
+            self.resolver = ipaddress.ip_address(info["resolver"])
+
+    def save(self, filename):
+        with open(filename, "w") as file:
+            json.dump({
+                "router": self.router,
+                "subnet": self.subnet,
+                "original_ip": self.original_ip,
+                "original_resolver": self.original_resolver,
+                "static_ip": self.static_ip,
+                "resolver": self.resolver
+            }, file)
+
+
 class Error(Exception):
     pass
 
@@ -29,36 +60,33 @@ def configure(use_info=False, self_as_resolver=False) -> None:
     if is_configured():
         return
     if use_info:
-        with open(INFO_FILE) as config_file:
-            info = json.load(config_file)
-        if len(info) == 0:
-            raise Error("Error using info: Info file is empty. Please try again without parameter.")
+        info = Info(filename=INFO_FILE)
     else:
-        info = {}
+        info = Info()
         # GET SUBNET INFO
         logging.info("Gathering local subnet and router information.")
         try:
             output = bash.call("ip route | "
                                "grep 'default' | "
                                "awk '{print $3, $7}'")
-            info["router"], info["original_ip"] = map(lambda x: ipaddress.ip_address(x), output.strip().split(" "))
-            info["subnet"] = ipaddress.ip_network(bash.call("ip route | "
-                                                            "grep -v 'default' | "
-                                                            "awk '{print $1}'").rstrip())
-            info["original_resolver"] = ipaddress.ip_address(bash.call("cat /etc/resolv.conf | "
-                                                                       "grep -m 1 'nameserver' | "
-                                                                       "awk '{print $2}'").rstrip())
+            info.router, info.original_ip = map(lambda x: ipaddress.ip_address(x), output.strip().split(" "))
+            info.subnet = ipaddress.ip_network(bash.call("ip route | "
+                                                         "grep -v 'default' | "
+                                                         "awk '{print $1}'").rstrip())
+            info.original_resolver = ipaddress.ip_address(bash.call("cat /etc/resolv.conf | "
+                                                                    "grep -m 1 'nameserver' | "
+                                                                    "awk '{print $2}'").rstrip())
         except bash.CallError as error:
             logging.critical(
                 "Critical error getting required local network information: {0}\n"
                 "Aborting now.".format(error))
             exit(-1)
         finally:
-            logging.info("Gathered information:\n"
-                         "IP-Address: {original_ip},\n"
-                         "Router: {router},\n"
-                         "Subnet: {subnet},\n"
-                         "Resolver: {original_resolver}".format(**info))
+            logging.info("\t\tGathered information:\n"
+                         "\t\tIP-Address: {info.original_ip},\n"
+                         "\t\tRouter: {info.router},\n"
+                         "\t\tSubnet: {info.subnet},\n"
+                         "\t\tResolver: {info.original_resolver}".format(info=info))
 
         # GET ACTIVE CLIENTS INFO
         logging.info("Scanning network for active host to get used addresses.")
@@ -66,41 +94,38 @@ def configure(use_info=False, self_as_resolver=False) -> None:
         try:
             output = bash.call("sudo nmap -sn -n {0} --exclude {1} | "
                                "grep 'scan report' | "
-                               "awk '{{print $5}}'".format(info["subnet"], info["original_ip"])).rstrip()
+                               "awk '{{print $5}}'".format(info.subnet, info.original_ip)).rstrip()
             devices = list(map(lambda x: ipaddress.ip_address(x), output.split("\n")))
         except bash.CallError as error:
             logging.error("Error scanning local network: {0}\n"
                           "Errors configuring static IP are possible.".format(error))
-            devices.append(info["router"])
+            devices.append(info.router)
         finally:
             logging.info("{0} active clients found.".format(len(devices)))
 
         # SELECT STATIC IP
         # TODO: Try to get DHCP range to be sure.
         logging.info("Selecting an unused static IP.")
-        for host in info["subnet"].hosts():
+        for host in info.subnet.hosts():
             if host not in devices:
-                info["static_ip"] = host
+                info.static_ip = host
                 break
-        logging.info("Static IP selected: {0}".format(info["static_ip"]))
+        logging.info("Static IP selected: {0}".format(info.static_ip))
 
         # CHOOSE RESOLVER
-        info["resolver"] = info["static_ip"] if self_as_resolver else info["original_resolver"]
+        info.resolver = info.static_ip if self_as_resolver else info.original_resolver
 
         # WRITE INFO TO FILE FOR OTHERS TO USE
-        for key, item in info.items():
-            info[key] = item.exploded
-        with open(INFO_FILE, "w") as info_file:
-            json.dump(info, info_file)
+        info.save(INFO_FILE)
 
     # SET STATIC IP
     logging.info("Changing DHCP Client Daemon to static IP address configuration.")
     shutil.copy2(DHCPCD_CONF, DHCPCD_CONF + ".original")
     logging.debug("Saved original dhcpcd.conf with suffix '.original'.")
     with open(DHCPCD_CONF, "a") as dhcpcd_conf:
-        static_conf = STATIC_DHCPCD_CONF.format(info["static_ip"],
-                                                info["router"],
-                                                info["resolver"])
+        static_conf = STATIC_DHCPCD_CONF.format(info.static_ip,
+                                                info.router,
+                                                info.resolver)
         dhcpcd_conf.write(static_conf)
     logging.debug("Appended static configuration to dhcpcd.conf file.")
 
